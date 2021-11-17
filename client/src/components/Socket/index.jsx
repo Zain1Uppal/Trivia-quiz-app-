53cdd98e18ecb659be508d78925be0c1d0ea88b5
Zain Uppal <zainuppal1@gmail.com>
2021-11-17 17:20:54 +0000

    random

--- client/src/components/Socket/index.jsx
+++ client/src/components/Socket/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Lobby } from '../../pages/index';
 import io from 'socket.io-client';
 import './style.css';
 
@@ -10,70 +9,67 @@ export const Socket = () => {
     const [login, setLogin] = useState(false)
     const [room, setRoom] = useState('')
     const [userName, setUsername] = useState('')
-    const [score, setScore] = useState(0)
-    const [questionList, setQuestions] = useState([])
+    const [count, setCount] = useState(0)
+    const [questionList, setQuestions]= useState([])
     const [endGame, setEndgame] = useState(false);
     const [startGame, setStart] = useState(false)
-    const [leaderboard, setLeaderboard] = useState([])
-    const [buttonShow, setButtonShow] = useState(false)
-    const [createRoom, setCreateRoom] = useState(false)
-    // const [admin, setAdmin] = useState(false);
 
     useEffect(() => {
         socket = io(CONNECTION_URL)
     }, [CONNECTION_URL]);
 
-
+    
     useEffect(() => {
-        const userData = [score, userName]
-        socket.emit('track_score', userData)
-    }, [score])
-
+        socket.emit('track_score', count)
+    }, [count])
 
     const connectRoom = () => {
         setLogin(true)
-        const data = [userName, score, room]
+        const data = [userName, count, room]
         socket.emit('all_data', data)
         socket.emit('join_room', room)
         socket.emit('username', userName)
     }
 
     useEffect(() => {
+        console.log('in useEffect')
         socket.on('receive_q', (data) => {
+            console.log('questions'+ JSON.stringify(data))
             setQuestions(JSON.stringify(data))
         })
-    }, [startGame])
+    },[startGame])
 
-    const genRoomId = () => {
-        let roomId = '';
-        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        let charLen = chars.length;
-        for (let i = 0; i < 6; i++) {
-            roomId += chars.charAt(Math.floor(Math.random() * charLen));
-        }
-        setLogin(true)
-        const data = [userName, score, roomId]
-        socket.emit('join_room', roomId)
-        socket.emit('all_data', data)
-        socket.emit('username', userName)
-        setRoom(roomId)
-        setCreateRoom(true)
+    function handleClick (){
+        setCount((prevCount) => prevCount + 1)
     }
 
-    return (
+    function handleEnd (){
+        setEndgame((prevEnd) => !prevEnd)
+    }
+
+    function handleStart(){
+        socket.emit('start_game', room)
+        setStart((prevEnd) => !prevEnd)
+    }
+    return(
         <div>
-            {!login ?
-                (<form className='roomJoin' id='roomJoin' action='/lobby'>
+            {!login ? (
+                <form className ='roomJoin' id='roomJoin'>
                     <h2>Enter your username and room number</h2>
                     <input placeholder='name' onChange={(e) => {
                         setUsername(e.target.value)
-                    }} />
+                    }}/>
                     <input placeholder='room' onChange={(e) => {
                         setRoom(e.target.value)
-                    }} />
-                    <button onClick={connectRoom} disabled={!(userName.length >= 3)} >Enter</button>
-                    <button onClick={genRoomId} disabled={!(userName.length >= 3)}>Create a Room</button>
-                </form>) : (<Lobby socket={socket} userName={userName} roomNum={room} createR={createRoom} />)}
+                    }}/>
+                    <button onClick={connectRoom}>Enter</button>
+                </form>):(<div>
+                    <button onClick={handleClick}>{count}</button>
+                    <button onClick={handleEnd}>end game</button>
+                    <button onClick={handleStart}>start game</button>
+                    <h1>questions is : {questionList}</h1>
+                </div>
+            )}
         </div>
     )
 }
